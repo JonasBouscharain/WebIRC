e115f4f694480e190391f031d3365b3897579c83
JonasBouscharain <bouscharain.jonas@hotmail.fr>
2015-12-10 23:00:28 +0100

    adding package.json for npm + updating the serveur

--- serveur.js
+++ serveur.js
@@ -1,46 +1,50 @@
-var http = require('http');
-var fs = require('fs');
+var path = require('path');
+var express = require('express');
+http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
 
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
+app.set('port', (process.env.PORT || 8080));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var server = http.createServer(app);
 
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
 
 var logFactory = function(type, msg){
-	var date = new Date();
-	console.log("["+date.getDate()+"/"+date.getMonth()+"/"+date.getYear()+", "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+"] "+ type + " -> " + msg);
+  var date = new Date();
+  console.log("["+addZero(date.getDate())+"/"+addZero(date.getMonth())+"/"+date.getFullYear()+", "+addZero(date.getHours())+":"+addZero(date.getMinutes())+":"+addZero(date.getSeconds())+"] "+ type + " -> " + msg);
+}
+
+var addZero = function(i){
+    var tmp = '';
+    (i<10)?tmp = '0'+i:tmp = i;
+    return tmp;
 }
 
 // Quand on client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
-	clientPseudo = "Anonym";
-    // console.log('Un client anonyme est en train de se connecter...');
+  socket.clientPseudo = "Anonym";
     logFactory("CONNECTION", "Pending, anonym");
 
-    // socket.emit('message', 'Vous êtes bien connecté !');
-
     socket.on('new', function (pseudo) {
-    	clientPseudo = pseudo;
-    	socket.emit('serv_msg', 'Bienvenue '+clientPseudo+' !');
-    	socket.broadcast.emit('message', clientPseudo+' vient de se connecter !');
-        // console.log('Un client me parle ! Il me dit : ' + message);
-        logFactory("CONNECTED", clientPseudo+" is connected");
+      socket.clientPseudo = pseudo;
+      socket.emit('serv_msg', '<tr><td></td><td>Bienvenue '+socket.clientPseudo+' !</td></tr>');
+      socket.broadcast.emit('message', '<tr><td></td><td>'+socket.clientPseudo+' vient de se connecter !</td></tr>');
+        logFactory("CONNECTED", socket.clientPseudo+" is connected");
     });
 
     socket.on('message', function (message) {
-    	socket.emit('message', clientPseudo+" : "+message);
-    	socket.broadcast.emit('message', clientPseudo+" : "+message);
-        // console.log('Un client me parle ! Il me dit : ' + message);
-        logFactory("CLIENTMSG", clientPseudo+" : "+message);
-    });	
+      socket.emit('message', '<tr><td>'+socket.clientPseudo+"</td><td>"+message+'</td></tr>');
+      socket.broadcast.emit('message', '<tr><td>'+socket.clientPseudo+"</td><td>"+message+'</td></tr>');
+        logFactory("CLIENTMSG", socket.clientPseudo+" : "+message);
+    }); 
 });
 
-
-
-server.listen(8080);
+server.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
